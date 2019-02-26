4e59556137912b9a927010af24a2574c5b26082a
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-26 18:41:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["Knarquelya", "Knartara", "Kathy", "Kanaja"]
+  create_an_array = ["wow", "Knartara", "Kathy", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
